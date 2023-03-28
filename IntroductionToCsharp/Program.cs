ddec9afe7360553b8192b7c8399d7aecb4e2d2ad
Asim-hameed <asimhameed31@gmail.com>
2023-03-28 22:56:05 +0500

    Add project files.

---
+++ IntroductionToCsharp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter first Name: ");
+string fname= Console.ReadLine();
+Console.WriteLine("Enter Last Name: ");
+string lname= Console.ReadLine(); 
+Console.WriteLine("Your name is {0} {1}",fname,lname);   //placeholder syntax
+Console.ReadLine();
